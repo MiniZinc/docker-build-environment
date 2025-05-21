8dbf687bcf9c5e8face6f48cffe2c582b91dbf1d
Jason N <cyderize@users.noreply.github.com>
2025-05-21 13:32:43 +1000

    Update to rockylinux 9

--- Dockerfile.cpp
+++ Dockerfile.cpp
@@ -1,13 +1,13 @@
-FROM rockylinux/rockylinux:8
+FROM rockylinux/rockylinux:9
 
 # install epel-release for ccache
 RUN dnf install -y epel-release && \
-	dnf install --enablerepo=powertools -y \
+	dnf install --enablerepo=crb -y \
 	bison \
 	ccache \
 	cmake \
-	gcc-toolset-12 \
-	gcc-toolset-12-gcc-c++ \
+	gcc-toolset-14 \
+	gcc-toolset-14-gcc-c++ \
 	findutils \
 	flex \
 	git \
@@ -16,6 +16,6 @@ RUN dnf install -y epel-release && \
 	subversion \
 	unzip \
 	zlib-devel && dnf clean all -y && rm -rf /var/cache/dnf
-ENV BASH_ENV=/opt/rh/gcc-toolset-12/enable \
-	ENV=/opt/rh/gcc-toolset-12/enable \
-	PROMPT_COMMAND=". /opt/rh/gcc-toolset-12/enable"
+ENV BASH_ENV=/opt/rh/gcc-toolset-14/enable \
+	ENV=/opt/rh/gcc-toolset-14/enable \
+	PROMPT_COMMAND=". /opt/rh/gcc-toolset-14/enable"
